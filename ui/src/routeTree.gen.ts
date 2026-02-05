16026c5755a916e306b9a9868092529e83d930cc
Krzysztof Bzowski <kbzowski@agh.edu.pl>
2026-02-05 14:39:57 +0100

    feat(ui): add image-preview window capability and route

--- ui/src/routeTree.gen.ts
+++ ui/src/routeTree.gen.ts
@@ -9,8 +9,14 @@
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
 import { Route as rootRouteImport } from './routes/__root'
+import { Route as ImagePreviewRouteImport } from './routes/image-preview'
 import { Route as IndexRouteImport } from './routes/index'
 
+const ImagePreviewRoute = ImagePreviewRouteImport.update({
+  id: '/image-preview',
+  path: '/image-preview',
+  getParentRoute: () => rootRouteImport,
+} as any)
 const IndexRoute = IndexRouteImport.update({
   id: '/',
   path: '/',
@@ -19,28 +25,39 @@ const IndexRoute = IndexRouteImport.update({
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
+  '/image-preview': typeof ImagePreviewRoute
 }
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
+  '/image-preview': typeof ImagePreviewRoute
 }
 export interface FileRoutesById {
   __root__: typeof rootRouteImport
   '/': typeof IndexRoute
+  '/image-preview': typeof ImagePreviewRoute
 }
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/'
+  fullPaths: '/' | '/image-preview'
   fileRoutesByTo: FileRoutesByTo
-  to: '/'
-  id: '__root__' | '/'
+  to: '/' | '/image-preview'
+  id: '__root__' | '/' | '/image-preview'
   fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
+  ImagePreviewRoute: typeof ImagePreviewRoute
 }
 
 declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
+    '/image-preview': {
+      id: '/image-preview'
+      path: '/image-preview'
+      fullPath: '/image-preview'
+      preLoaderRoute: typeof ImagePreviewRouteImport
+      parentRoute: typeof rootRouteImport
+    }
     '/': {
       id: '/'
       path: '/'
@@ -53,6 +70,7 @@ declare module '@tanstack/react-router' {
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
+  ImagePreviewRoute: ImagePreviewRoute,
 }
 export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
