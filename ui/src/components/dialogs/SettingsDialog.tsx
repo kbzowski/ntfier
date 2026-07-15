ffd298617bb29cc2b5ef204de9c1ce6b9f1a7963
Krzysztof Bzowski <kbzowski@agh.edu.pl>
2026-07-15 15:11:34 +0200

    fix: widen settings dialog to fit five tabs

--- ui/src/components/dialogs/SettingsDialog.tsx
+++ ui/src/components/dialogs/SettingsDialog.tsx
@@ -115,7 +115,7 @@ export function SettingsDialog({
 
 	return (
 		<Dialog open={open} onOpenChange={onOpenChange}>
-			<DialogContent className="sm:max-w-lg">
+			<DialogContent className="sm:max-w-2xl">
 				<DialogHeader>
 					<DialogTitle>Settings</DialogTitle>
 				</DialogHeader>
